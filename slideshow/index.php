e407152e7cce4559254ad33b484118a7b5cb9875
Orange <Orange.work122@gmail.com>
2026-03-09 03:10:23 +0700

    update

--- slideshow/index.php
+++ slideshow/index.php
@@ -14,18 +14,17 @@ $photoswipe = true;
 $randomImage = $config['slideshow']['randomPicture'];
 $remoteBuzzer = false;
 
+/** Use same gallery template as main app; title = "Slideshow" */
+$galleryTitle = 'slideshow';
+
 include PathUtility::getAbsolutePath('template/components/main.head.php');
 
 ?>
-<body>
-    <div id="gallery" class="gallery">
-        <div class="gallery-header">
-            <div class="gallery-title"><h1><?=$languageService->translate('slideshow')?></h1></div>
-        </div>
-        <div class="gallery-body">
-            <?php include PathUtility::getAbsolutePath('template/components/gallery.images.php'); ?>
-        </div>
-    </div>
+<body class="gallery-mode--overlay">
+    <?php include PathUtility::getAbsolutePath('template/components/gallery.php'); ?>
+    <script>
+        document.getElementById('gallery').classList.add('gallery--open');
+    </script>
     <?php include PathUtility::getAbsolutePath('template/components/main.footer.php'); ?>
     <script src="<?=$assetService->getUrl('resources/js/slideshow.js')?>"></script>
 </body>
--- template/components/gallery.php
+++ template/components/gallery.php
@@ -5,11 +5,13 @@ use Photobooth\Utility\ComponentUtility;
 use Photobooth\Utility\PathUtility;
 
 $languageService = LanguageService::getInstance();
+/** @var string $galleryTitle Optional: 'gallery' (default) or 'slideshow' for title translation key */
+$galleryTitle = $galleryTitle ?? 'gallery';
 
 ?>
 <div id="gallery" class="gallery rotarygroup">
     <div class="gallery-header">
-        <div class="gallery-title"><h1><?= $languageService->translate('gallery') ?></h1></div>
+        <div class="gallery-title"><h1><?= $languageService->translate($galleryTitle) ?></h1></div>
         <div class="gallery-actions">
             <?= ComponentUtility::renderButton('close', $config['icons']['close'], 'gallery__close') ?>
             <?= ComponentUtility::renderButton('reload', $config['icons']['refresh'], 'gallery__refresh', true, ['class' => 'hidden']) ?>
